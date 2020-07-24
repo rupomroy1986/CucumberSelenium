17bfc18f0cc6806bf87583c91951708cbc9ff1b1
rupomroy1986 <roy.rupom6@gmail.com>
2020-07-24 15:33:45 +0530

    added basic framework details

--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -39,7 +39,7 @@ formatter.match({
   "location": "MyStepDefinitions.user_is_on_greencart_landing_page()"
 });
 formatter.result({
-  "duration": 9658839284,
+  "duration": 11573861846,
   "status": "passed"
 });
 formatter.match({
@@ -52,7 +52,7 @@ formatter.match({
   "location": "MyStepDefinitions.user_searched_for_vegetable(String)"
 });
 formatter.result({
-  "duration": 5493942603,
+  "duration": 5762209280,
   "status": "passed"
 });
 formatter.match({
@@ -65,11 +65,272 @@ formatter.match({
   "location": "MyStepDefinitions.something_results_are_displayed(String)"
 });
 formatter.result({
-  "duration": 5063852319,
+  "duration": 5242923780,
   "status": "passed"
 });
 formatter.after({
-  "duration": 169767714,
+  "duration": 906170418,
+  "status": "passed"
+});
+formatter.scenarioOutline({
+  "line": 9,
+  "name": "Search for items and then move to checkout page",
+  "description": "",
+  "id": "search-and-place-order-for-vegetables;search-for-items-and-then-move-to-checkout-page",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 8,
+      "name": "@RegressionTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 10,
+  "name": "User is on Greencart Landing page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 11,
+  "name": "User searched for \u003cName\u003e Vegetable",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "Added items to cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "User proceeded to Checkout page for purchase",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "verify selected \u003cName\u003e items are displayed in Check out page",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 17,
+  "name": "",
+  "description": "",
+  "id": "search-and-place-order-for-vegetables;search-for-items-and-then-move-to-checkout-page;",
+  "rows": [
+    {
+      "cells": [
+        "Name"
+      ],
+      "line": 18,
+      "id": "search-and-place-order-for-vegetables;search-for-items-and-then-move-to-checkout-page;;1"
+    },
+    {
+      "cells": [
+        "Brinjal"
+      ],
+      "line": 19,
+      "id": "search-and-place-order-for-vegetables;search-for-items-and-then-move-to-checkout-page;;2"
+    },
+    {
+      "cells": [
+        "Beetroot"
+      ],
+      "line": 20,
+      "id": "search-and-place-order-for-vegetables;search-for-items-and-then-move-to-checkout-page;;3"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 19,
+  "name": "Search for items and then move to checkout page",
+  "description": "",
+  "id": "search-and-place-order-for-vegetables;search-for-items-and-then-move-to-checkout-page;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 8,
+      "name": "@RegressionTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 10,
+  "name": "User is on Greencart Landing page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 11,
+  "name": "User searched for Brinjal Vegetable",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "Added items to cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "User proceeded to Checkout page for purchase",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "verify selected Brinjal items are displayed in Check out page",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyStepDefinitions.user_is_on_greencart_landing_page()"
+});
+formatter.result({
+  "duration": 5353574969,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Brinjal",
+      "offset": 18
+    }
+  ],
+  "location": "MyStepDefinitions.user_searched_for_vegetable(String)"
+});
+formatter.result({
+  "duration": 5484621348,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepDefinitions.added_items_to_cart()"
+});
+formatter.result({
+  "duration": 623843070,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepDefinitions.user_proceeded_to_checkout_page_for_purchase()"
+});
+formatter.result({
+  "duration": 420947738,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Brinjal",
+      "offset": 16
+    }
+  ],
+  "location": "stepDefination.verify_selected_items_are_displayed_in_check_out_page(String)"
+});
+formatter.result({
+  "duration": 55852666,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 823778831,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 20,
+  "name": "Search for items and then move to checkout page",
+  "description": "",
+  "id": "search-and-place-order-for-vegetables;search-for-items-and-then-move-to-checkout-page;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 8,
+      "name": "@RegressionTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 10,
+  "name": "User is on Greencart Landing page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 11,
+  "name": "User searched for Beetroot Vegetable",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "Added items to cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "User proceeded to Checkout page for purchase",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "verify selected Beetroot items are displayed in Check out page",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyStepDefinitions.user_is_on_greencart_landing_page()"
+});
+formatter.result({
+  "duration": 5839319891,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Beetroot",
+      "offset": 18
+    }
+  ],
+  "location": "MyStepDefinitions.user_searched_for_vegetable(String)"
+});
+formatter.result({
+  "duration": 5642935371,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepDefinitions.added_items_to_cart()"
+});
+formatter.result({
+  "duration": 406491009,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepDefinitions.user_proceeded_to_checkout_page_for_purchase()"
+});
+formatter.result({
+  "duration": 442966404,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Beetroot",
+      "offset": 16
+    }
+  ],
+  "location": "stepDefination.verify_selected_items_are_displayed_in_check_out_page(String)"
+});
+formatter.result({
+  "duration": 59135844,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 186868858,
   "status": "passed"
 });
 });
